3d11bdd9508842d5e04bfbbcd133b979e4c023ce
Luis Tomas Bolivar <ltomasbo@redhat.com>
2024-04-09 12:44:28 +0200

    Add option to download the model from S3

--- openshift-ai/pipeline_nmvllm.py
+++ openshift-ai/pipeline_nmvllm.py
@@ -11,23 +11,63 @@ SPARSE_MODEL_DIR = BASE_DIR + "sparse-llm"
 QUANT_MODEL_DIR = BASE_DIR + "quant-llm"
 EXPORTED_MODEL_DIR = BASE_DIR + "exported"
 
-def download_model(model_name: str, destination_path: str):
-    import subprocess
+def download_model(model_name: str, destination_path: str,
+                   download_option: str):
+    if download_option == "HF":
+        import subprocess
+        print('Starting downloading the model from HF')
+        # Execute the huggingface_hub-cli command
+        result = subprocess.run(["huggingface-cli", "download", model_name,
+                                 "--local-dir", destination_path,
+                                 "--local-dir-use-symlinks", "False"],
+                                capture_output=True, text=True)
+        # Check for errors or output
+        if result.returncode == 0:
+            print("Model downloaded successfully from HF.")
+        else:
+            print("Error downloading model:")
+            print(result.stderr)
 
-    # Execute the huggingface_hub-cli command
-    result = subprocess.run(["huggingface-cli", "download", model_name,
-                             "--local-dir", destination_path,
-                             "--local-dir-use-symlinks", "False"], capture_output=True, text=True)
+    elif download_option == "S3":
+        import os
+        import errno
+        from boto3 import client
 
-    # Check for errors or output
-    if result.returncode == 0:
-        print("Model downloaded successfully.")
-    else:
-        print("Error downloading model:")
-        print(result.stderr)
+        print('Starting downloading the model from S3')
+ 
+        s3_endpoint_url = os.environ["s3_host"]
+        s3_access_key = os.environ["s3_access_key"]
+        s3_secret_key = os.environ["s3_secret_access_key"]
+        s3_bucket_name = os.environ["s3_bucket"]
 
+        s3_client = client(
+            's3', endpoint_url=s3_endpoint_url, aws_access_key_id=s3_access_key,
+            aws_secret_access_key=s3_secret_key, verify=False
+        )
 
-def sparse_model(model_path:str, compress_model_path: str, ds: str, sparsity_ratio: float):
+        # list all objects in the folder
+        objects = s3_client.list_objects(Bucket=s3_bucket_name, Prefix=model_name)
+
+        # download each object in the folder
+        for object in objects['Contents']:
+            file_name = object['Key']
+            local_file_name = os.path.join(destination_path, file_name.replace(model_name, '')[1:])
+            if not os.path.exists(os.path.dirname(local_file_name)):
+                try:
+                    os.makedirs(os.path.dirname(local_file_name))
+                except OSError as exc: # Guard against race condition
+                    if exc.errno != errno.EEXIST:
+                        print("Error downloading model")
+                        raise
+            print('Download file to')
+            print(file_name)
+            print(local_file_name)
+            s3_client.download_file(s3_bucket_name, file_name, local_file_name)
+
+        print('Model downloaded successfully from S3.')
+
+def sparse_model(model_path:str, compress_model_path: str, ds: str,
+                 sparsity_ratio: float):
     import sparseml.transformers
 
     recipe = f"""
@@ -109,7 +149,8 @@ def quantize_gpu_model(model_path:str, compress_model_path: str, ds: str):
     NUM_EXAMPLES = 512
 
     def preprocess(example):
-        return {"text": tokenizer.apply_chat_template(example["messages"], tokenize=False)}
+        return {"text": tokenizer.apply_chat_template(example["messages"],
+                                                      tokenize=False)}
 
     print("Loading the dataset and tokenizers")
     dataset = load_dataset(ds, split="train_sft")
@@ -119,7 +160,8 @@ def quantize_gpu_model(model_path:str, compress_model_path: str, ds: str):
 
     examples = [
         tokenizer(
-            example["text"], padding=False, max_length=MAX_SEQ_LEN, truncation=True,
+            example["text"], padding=False, max_length=MAX_SEQ_LEN,
+            truncation=True,
         ) for example in ds
     ]
 
@@ -205,8 +247,6 @@ def upload_model(model_path: str, name: str):
     from boto3 import client
 
     print('Starting results upload.')
-    print(os.environ)
-
     s3_endpoint_url = os.environ["s3_host"]
     s3_access_key = os.environ["s3_access_key"]
     s3_secret_key = os.environ["s3_secret_access_key"]
@@ -216,8 +256,8 @@ def upload_model(model_path: str, name: str):
           f'to S3 storage at {s3_endpoint_url}')
 
     s3_client = client(
-        's3', endpoint_url=s3_endpoint_url,
-        aws_access_key_id=s3_access_key, aws_secret_access_key=s3_secret_key, verify=False
+        's3', endpoint_url=s3_endpoint_url, aws_access_key_id=s3_access_key,
+        aws_secret_access_key=s3_secret_key, verify=False
     )
 
     # Walk through the local folder and upload files
@@ -233,7 +273,7 @@ def upload_model(model_path: str, name: str):
 
 
 download_op = comp.create_component_from_func(download_model,
-                                              packages_to_install=["huggingface-hub"],
+                                              packages_to_install=["huggingface-hub", "boto3"],
                                               base_image='registry.access.redhat.com/ubi9/python-311')
 sparse_op = comp.create_component_from_func(sparse_model,
                                             packages_to_install=["datasets"],
@@ -273,55 +313,74 @@ def cpu_model_optimization(predecing_task:object, model_path:str,
                                  compress_model_path=QUANT_MODEL_DIR,
                                  ds="open_platypus")
         quant_llm.add_pvolumes({"/mnt/models": vol})
-        quant_llm.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+        quant_llm.add_node_selector_constraint(
+            label_name='nvidia.com/gpu.present', value='true')
         quant_llm.add_toleration(gpu_toleration)
         quant_llm.add_resource_request('nvidia.com/gpu', "1")
         quant_llm.add_resource_limit('nvidia.com/gpu', "1")
         quant_llm.after(predecing_task)
 
         with dsl.Condition(eval == True):
-            eval_llm = eval_op(model_path=QUANT_MODEL_DIR, tasks=eval_task, batch_size=eval_batch_size)
+            eval_llm = eval_op(model_path=QUANT_MODEL_DIR, tasks=eval_task,
+                               batch_size=eval_batch_size)
             eval_llm.add_pvolumes({"/mnt/models": vol})
-            eval_llm.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+            eval_llm.add_node_selector_constraint(
+                label_name='nvidia.com/gpu.present', value='true')
             eval_llm.add_toleration(gpu_toleration)
             eval_llm.add_resource_request('nvidia.com/gpu', "1")
             eval_llm.add_resource_limit('nvidia.com/gpu', "1")
             eval_llm.after(quant_llm)
 
-        export_llm = export_op(model_path=QUANT_MODEL_DIR, exported_model_path=EXPORTED_MODEL_DIR)
+        export_llm = export_op(model_path=QUANT_MODEL_DIR,
+                               exported_model_path=EXPORTED_MODEL_DIR)
         export_llm.add_pvolumes({"/mnt/models": vol})
         export_llm.after(quant_llm)
 
         with dsl.Condition(save_model == True):
-            upload_pruned_llm = upload_op(model_path=EXPORTED_MODEL_DIR, name=save_folder_name)
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_secret_access_key', 'aws-connection-models', 'AWS_SECRET_ACCESS_KEY'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
+            upload_pruned_llm = upload_op(model_path=EXPORTED_MODEL_DIR,
+                                          name=save_folder_name)
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_secret_access_key', 'aws-connection-models',
+                'AWS_SECRET_ACCESS_KEY'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
             upload_pruned_llm.add_pvolumes({"/mnt/models": vol})
             upload_pruned_llm.after(export_llm)
 
     with dsl.Condition(quantize == False):
         with dsl.Condition(eval == True):
             with dsl.Condition(sparse == True):
-                eval_llm = eval_op(model_path=model_path, tasks=eval_task, batch_size=eval_batch_size)
+                eval_llm = eval_op(model_path=model_path, tasks=eval_task,
+                                   batch_size=eval_batch_size)
                 eval_llm.add_pvolumes({"/mnt/models": vol})
-                eval_llm.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+                eval_llm.add_node_selector_constraint(
+                    label_name='nvidia.com/gpu.present', value='true')
                 eval_llm.add_toleration(gpu_toleration)
                 eval_llm.add_resource_request('nvidia.com/gpu', "1")
                 eval_llm.add_resource_limit('nvidia.com/gpu', "1")
                 eval_llm.after(predecing_task)
 
-        export_llm = export_op(model_path=model_path, exported_model_path=EXPORTED_MODEL_DIR)
+        export_llm = export_op(model_path=model_path,
+                               exported_model_path=EXPORTED_MODEL_DIR)
         export_llm.add_pvolumes({"/mnt/models": vol})
         export_llm.after(predecing_task)
 
         with dsl.Condition(save_model == True):
-            upload_pruned_llm = upload_op(model_path=EXPORTED_MODEL_DIR, name=save_folder_name)
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_secret_access_key', 'aws-connection-models', 'AWS_SECRET_ACCESS_KEY'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
+            upload_pruned_llm = upload_op(model_path=EXPORTED_MODEL_DIR,
+                                          name=save_folder_name)
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_secret_access_key', 'aws-connection-models',
+                'AWS_SECRET_ACCESS_KEY'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
             upload_pruned_llm.add_pvolumes({"/mnt/models": vol})
             upload_pruned_llm.after(export_llm)
 
@@ -339,47 +398,64 @@ def gpu_model_optimization(predecing_task:object, model_path:str,
                                  compress_model_path=QUANT_MODEL_DIR,
                                  ds="HuggingFaceH4/ultrachat_200k")
         quant_llm.add_pvolumes({"/mnt/models": vol})
-        quant_llm.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+        quant_llm.add_node_selector_constraint(
+            label_name='nvidia.com/gpu.present', value='true')
         quant_llm.add_toleration(gpu_toleration)
         quant_llm.add_resource_request('nvidia.com/gpu', "1")
         quant_llm.add_resource_limit('nvidia.com/gpu', "1")
         quant_llm.after(predecing_task)
 
         with dsl.Condition(eval == True):
-            eval_llm = eval_op(model_path=QUANT_MODEL_DIR, tasks=eval_task, batch_size=eval_batch_size)
+            eval_llm = eval_op(model_path=QUANT_MODEL_DIR, tasks=eval_task,
+                               batch_size=eval_batch_size)
             eval_llm.add_pvolumes({"/mnt/models": vol})
-            eval_llm.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+            eval_llm.add_node_selector_constraint(
+                label_name='nvidia.com/gpu.present', value='true')
             eval_llm.add_toleration(gpu_toleration)
             eval_llm.add_resource_request('nvidia.com/gpu', "1")
             eval_llm.add_resource_limit('nvidia.com/gpu', "1")
             eval_llm.after(quant_llm)
 
         with dsl.Condition(save_model == True):
-            upload_pruned_llm = upload_op(model_path=QUANT_MODEL_DIR, name=save_folder_name)
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_secret_access_key', 'aws-connection-models', 'AWS_SECRET_ACCESS_KEY'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
+            upload_pruned_llm = upload_op(model_path=QUANT_MODEL_DIR,
+                                          name=save_folder_name)
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_secret_access_key', 'aws-connection-models',
+                'AWS_SECRET_ACCESS_KEY'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
             upload_pruned_llm.add_pvolumes({"/mnt/models": vol})
             upload_pruned_llm.after(quant_llm)
     
     with dsl.Condition(quantize == False):
         with dsl.Condition(eval == True):
             with dsl.Condition(sparse == True):
-                eval_llm = eval_op(model_path=model_path, tasks=eval_task, batch_size=eval_batch_size)
+                eval_llm = eval_op(model_path=model_path, tasks=eval_task,
+                                   batch_size=eval_batch_size)
                 eval_llm.add_pvolumes({"/mnt/models": vol})
-                eval_llm.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+                eval_llm.add_node_selector_constraint(
+                    label_name='nvidia.com/gpu.present', value='true')
                 eval_llm.add_toleration(gpu_toleration)
                 eval_llm.add_resource_request('nvidia.com/gpu', "1")
                 eval_llm.add_resource_limit('nvidia.com/gpu', "1")
                 eval_llm.after(predecing_task)
 
         with dsl.Condition(save_model == True):
-            upload_pruned_llm = upload_op(model_path=model_path, name=save_folder_name)
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_secret_access_key', 'aws-connection-models', 'AWS_SECRET_ACCESS_KEY'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
-            upload_pruned_llm.add_env_variable(env_from_secret('s3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
+            upload_pruned_llm = upload_op(model_path=model_path,
+                                          name=save_folder_name)
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_secret_access_key', 'aws-connection-models',
+                'AWS_SECRET_ACCESS_KEY'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
+            upload_pruned_llm.add_env_variable(env_from_secret(
+                's3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
             upload_pruned_llm.add_pvolumes({"/mnt/models": vol})
             upload_pruned_llm.after(predecing_task)
 
@@ -391,7 +467,8 @@ def gpu_model_optimization(predecing_task:object, model_path:str,
 )
 def sparseml_pipeline(
     model_name:str="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    inference_target:str='CPU',
+    inference_target:str='CPU',  # CPU or GPU
+    download_option:str='HF',   # HF or S3
     shared_volume:str='models-shared',
     sparse:bool=True,
     sparsity_ratio:float=0.5,
@@ -416,7 +493,17 @@ def sparseml_pipeline(
                                   value='true')
 
     # Download volumes
-    download_llm = download_op(model_name, destination_path=MODEL_DIR)
+    download_llm = download_op(model_name, destination_path=MODEL_DIR,
+                               download_option=download_option)
+    download_llm.add_env_variable(env_from_secret(
+        's3_access_key', 'aws-connection-models', 'AWS_ACCESS_KEY_ID'))
+    download_llm.add_env_variable(env_from_secret(
+        's3_secret_access_key', 'aws-connection-models',
+        'AWS_SECRET_ACCESS_KEY'))
+    download_llm.add_env_variable(env_from_secret(
+        's3_host', 'aws-connection-models', 'AWS_S3_ENDPOINT'))
+    download_llm.add_env_variable(env_from_secret(
+        's3_bucket', 'aws-connection-models', 'AWS_S3_BUCKET'))
     download_llm.add_pvolumes({"/mnt/models": vol})
 
     sparse_llm = None
@@ -427,7 +514,8 @@ def sparseml_pipeline(
                                ds="open_platypus",
                                sparsity_ratio=sparsity_ratio)
         sparse_llm.add_pvolumes({"/mnt/models": vol})
-        sparse_llm.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+        sparse_llm.add_node_selector_constraint(
+            label_name='nvidia.com/gpu.present', value='true')
         sparse_llm.add_toleration(gpu_toleration)
         sparse_llm.add_resource_request('nvidia.com/gpu', "1")
         sparse_llm.add_resource_limit('nvidia.com/gpu', "1")
@@ -457,9 +545,11 @@ def sparseml_pipeline(
                                    gpu_toleration)
             
     with dsl.Condition(eval == True):
-        eval_llm_base = eval_op(model_path=MODEL_DIR, tasks=eval_task, batch_size=eval_batch_size)
+        eval_llm_base = eval_op(model_path=MODEL_DIR, tasks=eval_task,
+                                batch_size=eval_batch_size)
         eval_llm_base.add_pvolumes({"/mnt/models": vol})
-        eval_llm_base.add_node_selector_constraint(label_name='nvidia.com/gpu.present', value='true')
+        eval_llm_base.add_node_selector_constraint(
+            label_name='nvidia.com/gpu.present', value='true')
         eval_llm_base.add_toleration(gpu_toleration)
         eval_llm_base.add_resource_request('nvidia.com/gpu', "1")
         eval_llm_base.add_resource_limit('nvidia.com/gpu', "1")
