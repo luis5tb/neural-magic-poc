98e792ebd0f68333f0cae9529221a941cf1995a1
Luis Tomas Bolivar <ltomasbo@redhat.com>
2024-04-26 16:48:59 +0200

    Few extensions to the request.py

--- openshift-ai/request.py
+++ openshift-ai/request.py
@@ -15,8 +15,8 @@ MODEL_VAR = "/var/models"
 
 def get_answer(question, url):
     client = OpenAI(base_url=url, api_key="EMPTY")
-    #model = client.models.list().data[0][1]
-    model = client.models.list().data[0].id
+    model = client.models.list().data[0][1]
+    #model = client.models.list().data[0].id
     print(f"Accessing model API '{model}'")
 
     #completion = client.completions.create(model=model, prompt=question, max_tokens=100, temperature=0.2)
@@ -26,8 +26,8 @@ def get_answer(question, url):
     completion = client.completions.create(
         model=model,
         prompt=prompt,
-        max_tokens=500,
-        temperature=0,
+        max_tokens=1000,
+        temperature=0.2,
         n=1,
         stream=stream)
 
@@ -37,8 +37,33 @@ def get_answer(question, url):
     else:
         print(f"\n----- Prompt:\n{prompt}")
         print(f"\n----- Completion:\n{completion.choices[0].text}")
-    
+
     return completion.choices[0].text
+
+
+def get_answer_chat(question, url):
+    client = OpenAI(base_url=url, api_key="EMPTY")
+    model = client.models.list().data[0][1]
+    print(f"Accessing model API '{model}'")
+
+    msg = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": question}
+    ]
+
+    completion = client.chat.completions.create(
+        model=model,
+        messages=msg,
+        stream=True,
+        max_tokens=1000,
+        temperature=0.2)
+    #return completion.choices[0].message.content
+    response_content = []
+    for chunk in completion:
+        chunk_message = chunk.choices[0].delta
+        if chunk_message.content:
+            response_content.append(chunk_message.content)
+    return ''.join(response_content)
 
 
 def get_answer_req(question, url, model):
@@ -47,11 +72,12 @@ def get_answer_req(question, url, model):
 
     data = {
         "model": model,
-        "max_tokens": 1000,
-        "temperature": 0,
+        "max_tokens": 1024,
+        "temperature": 0.2,
         "messages": [
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": question}
+            {"role": "user", "content": "Use less than 300 words. " + question}
+            #{"role": "user", "content": question}
         ]}
     resp = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
 
@@ -91,12 +117,13 @@ def get_answer_req2(question, url, model):
         print("Error:", resp.status_code, resp.text)
         return None
 
-#iface = gr.Interface(fn=get_answer,
+##iface = gr.Interface(fn=get_answer,
+#iface = gr.Interface(fn=get_answer_chat,
 #                     inputs=["text", gr.Dropdown(choices=[URL])],
 #                     outputs="text")
 
-#iface = gr.Interface(fn=get_answer_req,
-iface = gr.Interface(fn=get_answer_req2,
+iface = gr.Interface(fn=get_answer_req,
+#iface = gr.Interface(fn=get_answer_req2,
 #                     inputs=["text", gr.Dropdown(choices=[URL]), gr.Dropdown(choices=[MODEL])],
                      inputs=["text", gr.Dropdown(choices=[URL]), gr.Dropdown(choices=[MODEL, MODEL_AUX, MODEL_VAR])],
                      outputs="text")
