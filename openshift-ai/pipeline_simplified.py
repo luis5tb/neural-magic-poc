16c2b228f5cf21b36ffdd9c354403b70e2d5ef2b
Luis Tomas Bolivar <ltomasbo@redhat.com>
2024-04-30 09:40:23 +0200

    Add memory request to export function to ensure it does not crash

--- openshift-ai/pipeline_simplified.py
+++ openshift-ai/pipeline_simplified.py
@@ -341,6 +341,10 @@ def cpu_model_optimization(predecing_task:object, sparsity_ratio:float,
     export_llm = export_op(model_path=COMPRESS_MODEL_DIR,
                            exported_model_path=EXPORTED_MODEL_DIR)
     export_llm.add_pvolumes({"/mnt/models": vol})
+    export_llm.add_resource_request('nvidia.com/gpu', "1")
+    export_llm.add_resource_limit('nvidia.com/gpu', "1")
+    export_llm.add_resource_request('memory', "32Gi")
+    export_llm.add_resource_limit('memory', "32Gi")
     export_llm.after(sparse_llm)
 
     with dsl.Condition(eval == True):
